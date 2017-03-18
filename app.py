c1c837e35672cb6d1c79707ebcd036909c870b81
Josh S <josh@joshsomma.com>
2017-03-18 16:49:49 +0000

    added football data api connector, just testing for a response here

--- app.py
+++ app.py
@@ -34,30 +34,16 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "getNextMatch":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
+    connection = http.client.HTTPConnection('api.football-data.org')
+    headers = {'X-Auth-Token': '11b3ad4cfece46049d2cc128c72f6104', 'X-Response-Control': 'minified'}
+    connection.request('GET', '/v1/teams/66', None, headers)
+    result = json.loads(connection.getresponse().read().decode())
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
-
-
-#def makeYqlQuery(req):
-    #result = req.get("result")
-    #parameters = result.get("parameters")
-    #city = parameters.get("geo-city")
-    #if city is None:
-        #return None
-
-    #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
 
 def makeWebhookResult(data):
     query = data.get('query')
@@ -84,8 +70,9 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             #", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Got something"
 
     print("Response:")
     print(speech)
